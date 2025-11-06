7d77f25a5715b9c69c76536468d37ef1f119b34d
fmalagoli <fabio.malagoli@energee3.com>
2025-11-06 19:02:56 +0100

    Aggiungere i file di progetto.

---
+++ FleetManager/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FleetManager.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
