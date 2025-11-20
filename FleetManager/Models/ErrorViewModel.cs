12d7ba84c29f225b7905b334593c8e405279c71f
alessio481 <picciatialessio2@gmail.com>
2025-11-20 10:47:01 +0100

    Cancellazione cose inutili

--- FleetManager/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace FleetManager.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
