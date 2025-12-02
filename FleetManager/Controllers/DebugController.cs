450f41ac74a45cbf8a04e42b701a07b6a415a5fb
alessio481 <picciatialessio2@gmail.com>
2025-12-02 10:15:03 +0100

    Reapply "Aggiunta qualche prova di controller, da cambiare perche da integrare con services e repository. Ogni volta che si entra fare update-database, cerca su google per capre cosa intendo. Per inserire dati di prova andare sulla pagina InserisciTantiDati"
    This reverts commit 642f907bca1c6afab84eba43f1090855cfbdb3c8.

--- FleetManager/Controllers/DebugController.cs
+++ FleetManager/Controllers/DebugController.cs
@@ -12,10 +12,20 @@ namespace FleetManager.Controllers
             _context = context;
         }
 
+        public IActionResult ciao() // Pagina/Debug/ciao
+        {
+            return Content("ciao");
+        }
+        public IActionResult Index() //Prova debug. Prima di andare a Pagina/Debug vedere di avere dati sul db
+        {
+            var veicoli = _context.Veicoli.ToList();
+            return View(veicoli);
+        }
+
         public IActionResult InserisciTantiDati()
         {
             // ================================
-            // 1) UTENTI            DA ELIMINARE dopo aver creato una migrazione e fattto l'update 
+            // 1) UTENTI            DA ELIMINARE dopo aver fatto l'update del server sql  
             // ================================
 
             var utenti = new List<Utente>
